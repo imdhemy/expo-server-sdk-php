1450603ce76a4a4ebf13bc292cf6b9b38f09fb2b
imdhemy <imdhemy@gmail.com>
2021-01-28 23:19:12 +0200

    Add MessageAble Interface

---
+++ src/Contracts/ExpoTokenAble.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace Imdhemy\Expo\Contracts;
+
+/**
+ * Interface ExpoTokenAble
+ * @package Imdhemy\Expo\Contracts
+ */
+interface ExpoTokenAble
+{
+    /**
+     * @return string
+     */
+    public function __toString(): string;
+}
